43c4151f730daa2ff3db171a30bb09387fed3c50
Romi Maccarini <romi@clay.cl>
2020-07-29 23:59:42 -0400

    create model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/dashboard'
+  get 'posts/create'
+  get 'posts/index'
+
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
